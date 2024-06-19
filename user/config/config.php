fad2051a0de31e884360a493aca0c21fae7abad1
fatihlhz <alfatih.hergun@gmail.com>
2024-06-19 19:54:15 +0700

    Add user side

---
+++ user/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_PATH', __DIR__ . '/../../database/db_tugas');
+
+try {
+    $pdo = new PDO('sqlite:' . DB_PATH);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Database connection failed: ' . $e->getMessage());
+}
+
+?>
